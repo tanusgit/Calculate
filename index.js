2616e71e90b8f2237b35eb287e27d2d4e429891f
Tanu <24976766+tanusgit@users.noreply.github.com>
2020-01-13 12:23:56 -0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,18 @@
+
+function change(){
+    var paragraph = document.getElementById("n").style.color ="blue";
+}
+//change();
+//document.write("5+4 = ", 5 + 4, <br />);
+//let subject = prompt("What do you want to learn today?");
+   // if(subject.equals("hitory"))
+   // {
+      // change();
+  //  }
+   // else paragraph = document.getElementsByID("Geo").innerHTML; 
+    //else if (subject.equals("geography")) {
+    //    var paragraph = document.getElementById("Geo").innerHTML; 
+    //} 
+   // else {
+    //    alert("Invalid subject. please enter a valid name");
+    //}
